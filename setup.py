ce1f9fd3d24f0ebd92ebfa0c954164493112da96
SoftwareKater <softwarekater@gmail.com>
2022-03-31 17:09:53 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='python-comdirect-client',
+    version='0.1.0',
+    py_modules=['src'],
+    install_requires=[
+        'appdirs',
+        'click',
+        'requests',
+    ],
+    entry_points={
+        'console_scripts': [
+            'pcc = src:cli',
+        ],
+    },
+)
